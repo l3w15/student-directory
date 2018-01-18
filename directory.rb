56e5f4200e5e6f01b80c97382d0afd33162e253c
Lewis <lewwwis.watson@gmail.com>
2018-01-18 14:22:14 +0000

    Add write to students.csv and gitignore it

--- directory.rb
+++ directory.rb
@@ -27,6 +27,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -36,6 +38,7 @@ end
 def print_menu
   puts "1. Add new student(s)"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end  
 def show_students
@@ -93,6 +96,17 @@ def print_students_list
 end
 def print_footer
   puts "Overall, we have #{@students.count} great students".center(50)
+end  
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end  
 # nothing happens until we call the methods
 interactive_menu
