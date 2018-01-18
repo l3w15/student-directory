e79cb61b4fe932ad1bf2c58ef6c72d2b35ec5c71
Lewis <lewwwis.watson@gmail.com>
2018-01-18 14:35:51 +0000

    Add load functionality for students.csv

--- directory.rb
+++ directory.rb
@@ -29,6 +29,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -39,6 +41,7 @@ def print_menu
   puts "1. Add new student(s)"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end  
 def show_students
@@ -60,7 +63,7 @@ def input_students
   #while the name is not empty, repeat this code
   while !name.empty? do
     puts "Please enter the student's cohort month or just hit enter"
-    month = gets.chomp.downcase
+    month = gets.chomp.downcase9
     month = "january" if month.empty?
     begin
        Date.parse(month)
@@ -105,6 +108,14 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end  
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end  
